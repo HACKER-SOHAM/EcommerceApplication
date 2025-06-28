79b68919096c98594de9b35b0fe794af410d6998
HACKER-SOHAM <72268451+HACKER-SOHAM@users.noreply.github.com>
2025-06-28 07:16:47 +0530

    Day2 adding e commerce project

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className= 'flex flex-col sm:flex-row border border-gray-400'>
       {/*Hero Left side*/}
       <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
-      <div className='text-C'>
+      <div className='text-center'>
         <div className='flex items-center gap-2'>
           <p className='w-9 md:w-11 h-[2px] bg-[#414141]'></p>
             <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
