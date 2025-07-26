2b89a3f4e04de17f3cfc7d2335f58ac720d92c9a
HACKER-SOHAM <72268451+HACKER-SOHAM@users.noreply.github.com>
2025-07-27 04:49:14 +0530

    some backend and admin started

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -54,16 +54,30 @@ const listProduct=async (req,res)=>{
      const products=await productModel.find({})
      res.json({success:true,products})
    }catch(error){
-
+    console.log(error)
+    res.json({success:false,message:error.message})
    }
 }
 //function for removing product
 const removeProduct=async (req,res)=>{
-
+    try{
+      await productModel.findByIdAndDelete(req.body.id)
+      res.json({success:true,message:"Product Removed"})
+    }catch(error){
+      console.log(error)
+      res.json({success:false,message:error.message})
+     }
 }
 //function for single product info
-const singleProduct=async()=>{
-
+const singleProduct=async(req,res)=>{
+  try{
+    const {productId}=req.body
+    const product=await productModel.findById(productId)
+    res.json({success:true,product})
+  }catch(error){
+    console.log(error)
+    res.json({success:false,message:error.message})
+  }
 }
 
 export {listProduct,addProduct,singleProduct,removeProduct}
