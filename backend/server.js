6f4963c503a83229854530ebecedb4382053eed7
HACKER-SOHAM <72268451+HACKER-SOHAM@users.noreply.github.com>
2025-07-26 05:51:51 +0530

    some more backend login addproduct etc

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import cors from 'cors'
 import 'dotenv/config'
 import ConnectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
-
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 //App Config
 const app=express()
 const port=process.env.PORT || 4000
@@ -12,6 +13,11 @@ connectCloudinary()
 //middlewares
 app.use(express.json())
 app.use(cors())
+
+//api endpoints
+
+app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
 
 //api
 app.get('/', (req, res) => {
